ddad1a1feb56aab15d968ce3ffab56a44cdb3a95
Ivan Avramov <iavramov@futurewei.com>
2020-08-17 14:01:07 -0700

    add k2 app

--- src/k2/postgres/src/backend/main/main_cpp_wrapper.cc
+++ src/k2/postgres/src/backend/main/main_cpp_wrapper.cc
@@ -1,7 +1,9 @@
+#include <k2/appbase/Appbase.h>
 extern "C" {
 int PostgresServerProcessMain(int argc, char** argv);
 }
 
 int main(int argc, char** argv) {
+	k2::App("PG");
 	return PostgresServerProcessMain(argc, argv);
 }
