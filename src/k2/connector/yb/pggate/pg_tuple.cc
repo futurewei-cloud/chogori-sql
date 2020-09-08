2231791b259fb25f2fce1c0a2467e399e35bccb8
johnfangAFW <jfang@futurewei.com>
2020-09-08 12:27:17 -0700

    port pg_tuple to represent a row of data

---
+++ src/k2/connector/yb/pggate/pg_tuple.cc
@@ -0,0 +1,78 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+// The following only applies to changes made to this file as part of YugaByte development.
+//
+// Portions Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+// Copyright(c) 2020 Futurewei Cloud
+//
+// Permission is hereby granted,
+//        free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all copies
+// or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS",
+// WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+//        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//        DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#include "yb/common/ybc-internal.h"
+#include "yb/pggate/pg_tuple.h"
+
+namespace k2 {
+namespace gate {
+
+PgTuple::PgTuple(uint64_t *datums, bool *isnulls, PgSysColumns *syscols)
+    : datums_(datums), isnulls_(isnulls), syscols_(syscols) {
+}
+
+void PgTuple::WriteNull(int index) {
+  isnulls_[index] = true;
+  datums_[index] = 0;
+}
+
+void PgTuple::WriteDatum(int index, uint64_t datum) {
+  isnulls_[index] = false;
+  datums_[index] = datum;
+}
+
+void PgTuple::Write(uint8_t **pgbuf, const uint8_t *value, int64_t bytes) {
+  // TODO: return a status instead of crashing.
+  CHECK_LE(bytes, kYBCMaxPostgresTextSizeBytes);
+  CHECK_GE(bytes, 0);
+  *pgbuf = static_cast<uint8_t*>(YBCCStringToTextWithLen(reinterpret_cast<const char*>(value),
+                                                         static_cast<int>(bytes)));
+}
+
+}  // namespace gate
+}  // namespace k2
