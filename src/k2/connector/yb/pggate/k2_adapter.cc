b398b8d876772d7e6e51b99f298b49754103e58e
Justin Funston <justin.funston@futurewei.com>
2020-11-18 23:46:13 +0000

    K2 adapter GetRowID implementation

--- src/k2/connector/yb/pggate/k2_adapter.cc
+++ src/k2/connector/yb/pggate/k2_adapter.cc
@@ -36,26 +36,102 @@ std::future<Status> K2Adapter::Exec(std::shared_ptr<K23SITxn> k23SITxn, std::sha
   //   a) populate the response object in op
   //   b) populate the data field in op as result set
   //   c) set the value for future
-  throw new std::logic_error("Not implemented yet");
+  throw std::logic_error("Not implemented yet");
 }
 
 std::future<Status> K2Adapter::BatchExec(std::shared_ptr<K23SITxn> k23SITxn, const std::vector<std::shared_ptr<PgOpTemplate>>& ops) {
   // same as the above except that send multiple requests and need to handle multiple futures from SKV
   // but only return a single future to this method caller
   // TODO: add implementation
-  throw new std::logic_error("Not implemented yet");
+  throw std::logic_error("Not implemented yet");
 }
 
 std::string K2Adapter::GetRowId(std::shared_ptr<SqlOpWriteRequest> request) {
-  // TODO: add implementation
-  // either use the virtual row id defined in ybctid_column_value field
-  // if it has been set or calculate the row id based on primary key values
-  // in key_column_values in the request
-  throw new std::logic_error("Not implemented yet");
+    // either use the virtual row id defined in ybctid_column_value field
+    // if it has been set or calculate the row id based on primary key values
+    // in key_column_values in the request
+
+    if (request->ybctid_column_value) {
+        if (!request->ybctid_column_value->isValueType()) {
+            throw std::logic_error("Non value type in ybctid_column_value");
+        }
+
+        std::shared_ptr<SqlValue> value = request->ybctid_column_value->getValue();
+        if (value->type_ != SqlValue::ValueType::SLICE) {
+            throw std::logic_error("ybctid_column_value value is not a Slice");
+        }
+
+        return value->data_->slice_val_.ToBuffer();
+    }
+
+    k2::dto::SKVRecord record = MakeSKVRecordWithKeysSerialized(*request);
+    k2::dto::Key key = record.getKey();
+    // No range keys in SQL and row id only has to be unique within a table, so only need partitionKey
+    return key.partitionKey;
 }
 
 std::future<K23SITxn> K2Adapter::beginTransaction() {
   return k23si_->beginTxn(k2::K2TxnOptions{});
+}
+
+k2::dto::SKVRecord K2Adapter::MakeSKVRecordWithKeysSerialized(SqlOpWriteRequest& request) {
+    // TODO use namespace name and table name directly? How does secondary index fit into this?
+    std::future<k2::GetSchemaResult> schema_f = k23si_->getSchema(request.namespace_name, request.table_name, 
+                                                                  request.schema_version);
+    // TODO ok to use it synchronously?
+    k2::GetSchemaResult schema_result = schema_f.get();
+    // TODO how are we handling errors? throwing exceptions?
+    if (!schema_result.status.is2xxOK()) {
+        throw std::runtime_error("Failed to get schema");
+    }
+
+    std::shared_ptr<k2::dto::Schema>& schema = schema_result.schema;
+    k2::dto::SKVRecord record(request.namespace_name, schema);
+
+    if (request.ybctid_column_value) {
+        // Using a pre-stored and pre-serialized key, just need to skip key fields
+        record.skipNext(); // For table name
+        record.skipNext(); // For index id TODO still mapping it this way?
+        // Note, not using range keys for SQL
+        for (size_t i=0; i < schema->partitionKeyFields.size(); ++i) {
+            record.skipNext();
+        }
+    } else {
+        // Serialize key data into SKVRecord
+        record.serializeNext<k2::String>(request.table_name);
+        record.serializeNext<int16_t>(0); // TODO how to get index id?
+        // TODO ok to assume key fields are in order?
+        for (const std::shared_ptr<SqlOpExpr>& expr : request.key_column_values) {
+            if (!expr->isValueType()) {
+                throw std::logic_error("Non value type in key_column_values");
+            }
+
+            std::shared_ptr<SqlValue> value = expr->getValue();
+
+            // TODO can make a macro for this when we have another use case
+            switch (value->type_) {
+                case SqlValue::ValueType::BOOL:
+                    record.serializeNext<bool>(value->data_->bool_val_);
+                    break;
+                case SqlValue::ValueType::INT:
+                    record.serializeNext<int64_t>(value->data_->int_val_);
+                    break;
+                case SqlValue::ValueType::FLOAT:
+                    record.serializeNext<float>(value->data_->float_val_);
+                    break;
+                case SqlValue::ValueType::DOUBLE:
+                    record.serializeNext<double>(value->data_->double_val_);
+                    break;
+                case SqlValue::ValueType::SLICE:
+                    record.serializeNext<k2::String>(k2::String(value->data_->slice_val_.ToBuffer()));
+                    break;
+                default:
+                    throw std::logic_error("Unknown SqlValue type");
+            }
+        }
+    }
+
+    return record;
 }
 
 K2Adapter::~K2Adapter() {
