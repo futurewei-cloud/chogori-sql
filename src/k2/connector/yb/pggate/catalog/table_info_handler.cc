6bc2e2cac78af513fa9dd15eb9b56930adf43113
johnfangAFW <jfang@futurewei.com>
2020-12-28 10:10:10 -0800

    change to use int64_t for all SKV integer types

--- src/k2/connector/yb/pggate/catalog/table_info_handler.cc
+++ src/k2/connector/yb/pggate/catalog/table_info_handler.cc
@@ -724,22 +724,22 @@ k2::dto::FieldType TableInfoHandler::ToK2Type(std::shared_ptr<SQLType> type) {
     k2::dto::FieldType field_type = k2::dto::FieldType::NOT_KNOWN;
     switch (type->id()) {
         case DataType::UINT8: {
-            field_type = k2::dto::FieldType::INT16T;
+            field_type = k2::dto::FieldType::INT64T;
         } break;
         case DataType::INT8: {
-            field_type = k2::dto::FieldType::INT16T;
+            field_type = k2::dto::FieldType::INT64T;
         } break;
         case DataType::UINT16: {
-            field_type = k2::dto::FieldType::INT32T;
+            field_type = k2::dto::FieldType::INT64T;
         } break;
         case DataType::INT16: {
-            field_type = k2::dto::FieldType::INT16T;
+            field_type = k2::dto::FieldType::INT64T;
         } break;
         case DataType::UINT32: {
             field_type = k2::dto::FieldType::INT64T;
         } break;
         case DataType::INT32: {
-            field_type = k2::dto::FieldType::INT32T;
+            field_type = k2::dto::FieldType::INT64T;
         } break;
         case DataType::UINT64: {
             field_type = k2::dto::FieldType::INT64T;
