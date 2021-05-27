e648b72e631897895ef5e6041ef3b75d6e98dcae
Justin Funston <justin.funston@futurewei.com>
2021-05-27 20:45:03 +0000

    Do not shutdown other clients on crash

--- pgtest/pg_run.sh
+++ pgtest/pg_run.sh
@@ -3,6 +3,7 @@ export LD_LIBRARY_PATH=/build/build/src/k2/connector/common/:/build/build/src/k2
 export YB_ENABLED_IN_POSTGRES=1
 export YB_PG_TRANSACTIONS_ENABLED=1
 export YB_PG_ALLOW_RUNNING_AS_ANY_USER=1
+export PG_NO_RESTART_ALL_CHILDREN_ON_CRASH=1
 
 export K2_CONFIG_FILE=/build/pgtest/k2config_pgrun.json
 
