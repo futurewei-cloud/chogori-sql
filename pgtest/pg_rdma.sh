5a90237facaa04ba94d5c9d19a8803f8c7ca7888
rvines <rvines@futurewei.com>
2021-07-01 22:32:50 +0000

    Removed hard coded paths

--- pgtest/pg_rdma.sh
+++ pgtest/pg_rdma.sh
@@ -4,12 +4,13 @@ echo "start container with"
 echo docker run --privileged --network=host -v "/dev/:/dev" -v "/sys/:/sys/" -v ${PWD}:/build -it --rm -e RDMAV_HUGEPAGES_SAFE=1  k2-bvu-10001.usrd.futurewei.com/k2sql_builder:latest
 echo ">>>>>>>>>>>>>>>>>>>>"
 
-export LD_LIBRARY_PATH=/build/build/src/k2/connector/common/:/build/build/src/k2/connector/entities/:/build/src/k2/postgres/lib
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export LD_LIBRARY_PATH=$SCRIPT_DIR/../build/src/k2/connector/common/:$SCRIPT_DIR/../build/src/k2/connector/entities/:$SCRIPT_DIR/../src/k2/postgres/lib
 export K2PG_ENABLED_IN_POSTGRES=1
 export K2PG_TRANSACTIONS_ENABLED=1
 export K2PG_ALLOW_RUNNING_AS_ANY_USER=1
 
-export K2_CONFIG_FILE=/build/pgtest/k2config_pgrun_rdma.json
+export K2_CONFIG_FILE=$SCRIPT_DIR/k2config_pgrun_rdma.json
 
 export GLOG_logtostderr=1 # log all to stderr
 export GLOG_v=5 #
@@ -19,4 +20,4 @@ cp pg_hba.conf pgroot/data/
 chown root pgroot/data/pg_hba.conf
 chmod 0700 pgroot/data/pg_hba.conf
 
-/build/src/k2/postgres/bin/postgres -D pgroot/data -p5433 -h "*"
+$SCRIPT_DIR/../src/k2/postgres/bin/postgres -D pgroot/data -p5433 -h "*"
--- pgtest/pg_run.sh
+++ pgtest/pg_run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/build/build/src/k2/connector/common/:/build/build/src/k2/connector/entities/:/build/src/k2/postgres/lib
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export LD_LIBRARY_PATH=$SCRIPT_DIR/../build/src/k2/connector/common/:$SCRIPT_DIR/../build/src/k2/connector/entities/:$SCRIPT_DIR/../src/k2/postgres/lib
 export K2PG_ENABLED_IN_POSTGRES=1
 export K2PG_TRANSACTIONS_ENABLED=1
 export K2PG_ALLOW_RUNNING_AS_ANY_USER=1
 export PG_NO_RESTART_ALL_CHILDREN_ON_CRASH=1
 
-export K2_CONFIG_FILE=/build/pgtest/k2config_pgrun.json
+export K2_CONFIG_FILE=$SCRIPT_DIR/k2config_pgrun.json
 
 export GLOG_logtostderr=1 # log all to stderr
 export GLOG_v=5 #
@@ -15,4 +16,4 @@ cp pg_hba.conf pgroot/data/
 chown root pgroot/data/pg_hba.conf
 chmod 0700 pgroot/data/pg_hba.conf
 
-/build/src/k2/postgres/bin/postgres -D pgroot/data -p5433 -h "*"
+$SCRIPT_DIR/../src/k2/postgres/bin/postgres -D pgroot/data -p5433 -h "*"
