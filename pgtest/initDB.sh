ff0a457ae1d0f8b1d9a5d066ee7e0c18dd1293c2
rvines <rvines@futurewei.com>
2021-07-01 22:52:49 +0000

    Added ld path

--- pgtest/initDB.sh
+++ pgtest/initDB.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-export LD_LIBRARY_PATH=$SCRIPT_DIR/../build/src/k2/connector/common/:$SCRIPT_DIR/../build/src/k2/connector/entities/
+export LD_LIBRARY_PATH=$SCRIPT_DIR/../build/src/k2/connector/common/:$SCRIPT_DIR/../build/src/k2/connector/entities/:$SCRIPT_DIR/../src/k2/postgres/lib
 export K2PG_ENABLED_IN_POSTGRES=1
 export K2PG_TRANSACTIONS_ENABLED=1
 export K2PG_ALLOW_RUNNING_AS_ANY_USER=1
--- pgtest/initDB_rdma.sh
+++ pgtest/initDB_rdma.sh
@@ -5,7 +5,7 @@ echo docker run --privileged --network=host -v "/dev/:/dev" -v "/sys/:/sys/" -v 
 echo ">>>>>>>>>>>>>>>>>>>>"
 
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-export LD_LIBRARY_PATH=$SCRIPT_DIR/../build/src/k2/connector/common/:$SCRIPT_DIR/../build/src/k2/connector/entities/
+export LD_LIBRARY_PATH=$SCRIPT_DIR/../build/src/k2/connector/common/:$SCRIPT_DIR/../build/src/k2/connector/entities/:$SCRIPT_DIR/../src/k2/postgres/lib
 export K2PG_ENABLED_IN_POSTGRES=1
 export K2PG_TRANSACTIONS_ENABLED=1
 export K2PG_ALLOW_RUNNING_AS_ANY_USER=1
